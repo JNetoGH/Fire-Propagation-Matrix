81dec5d06b29492a13cb29f9622299d770577058
JNeto <joaomendes15@gmail.com>
2024-09-09 22:55:46 +0100

    Matrix polished.

---
+++ Assets/Scripts/CellController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CellController : MonoBehaviour
+{
+    
+    public static int propagationsCounter = 0;
+    
+    [SerializeField] private CellStateEnum _cellState;
+    [SerializeField] public int lineIndex;
+    [SerializeField] public int columnIndex;
+    [SerializeField] public List<CellController> adjacentCells;
+    private Renderer _renderer;
+    
+    /// <summary>
+    /// Sets the state and the material at once
+    /// </summary>
+    public CellStateEnum CellState
+    {
+        get => _cellState;
+        set
+        {
+            _cellState = value;
+            _renderer.material = GridGenerator.Instance.StateToMaterial(value);
+        }
+    }
+
+    private void Awake()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+    
+    public void BurnCell() => CellState = CellStateEnum.Burnt;
+    
+    public void TryPropagateToAdjacentCells()
+    {
+        foreach (CellController adjacentCell in adjacentCells)
+        {
+            if (adjacentCell.CellState == CellStateEnum.Green)
+            {
+                adjacentCell.CellState = CellStateEnum.Fire;
+                propagationsCounter++;
+            }
+        }
+    }
+    
+}
